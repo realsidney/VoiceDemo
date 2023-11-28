64c8be8c4eb1c7c5bd076e8f7719c8d15362b020
Sidney <devwangyt@gmail.com>
2023-11-28 18:04:59 +0800

    Initial Commit

---
+++ VoiceDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  VoiceDemo
+//
+//  Created by wyt_M1 on 11/28/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ VoiceDemo/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  VoiceDemo
+//
+//  Created by wyt_M1 on 11/28/23.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+
+@end
